6e4f84e39156bd5828877abae61dc1e41eae6be1
Maximilian Stocker <max.stocker@gmx.de>
2025-09-07 16:57:56 +0200

    formated cpp file

--- madlock.cpp
+++ madlock.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main(int argc, char **argv) {
-    std::cout << "everything works" << std::endl;
-        return 0;
+  std::cout << "everything works" << std::endl;
+  return 0;
 }
